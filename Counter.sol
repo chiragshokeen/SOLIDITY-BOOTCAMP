39f321b32a4d5610076940242f8091f87c4d4a13
"chiragshokeen" <"chirag.shokeen007@gmail.com">
2022-01-06 13:06:35 +0530

    commit

--- Counter.sol
+++ Counter.sol
@@ -1,1 +1,26 @@
-pragma solidity ^0.6.0 ;
+pragma solidity ^0.6.0;
+
+contract Counter{
+
+    uint count ; //unsigned integer cannot be negative
+//this is state variable  - actually stored in blockchain
+
+    constructor() public { //run only once
+        count = 0 ; 
+    }
+
+    function getCount() public view returns(uint) { // to make function accessible outside the smartcontract use visibilty public
+    //specifies what kind of value will be returned
+        return count ; 
+
+    }
+
+    function incrementCount() public {
+        count = count+1 ; 
+    }
+
+
+
+
+
+}
